885322012e96b14758dfe4c0b053ab874e7316d2
TchiniangaB <bastientchinianga@gmail.com>
2025-04-21 22:08:49 +0200

    Initial commit: Single Cycle RISC-V processor

---
+++ Single_Cycle.v
@@ -0,0 +1,19 @@
+// Program Counter 
+module ProgramCounter (
+    clk, reset, PC_in, PC_out
+);
+
+input clk, reset;
+input       [31 : 0] PC_in;
+output reg  [31 : 0] PC_out;
+
+always @(posedge clk or posedge reset) begin
+
+    if (reset) begin
+        PC_out <= 32'b0;
+    end else begin
+        PC_out <= PC_in;
+    end
+end
+
+endmodule
