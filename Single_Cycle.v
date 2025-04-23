fcd1f77e79e58fabd310bcb4d93f74e78cb4cf0d
babaye06 <bastientchinianga@gmail.com>
2025-04-23 02:12:19 +0200

    Module Reg_File

--- Single_Cycle.v
+++ Single_Cycle.v
@@ -14,6 +14,7 @@ always @(posedge clk or posedge reset) begin
     end else begin
         PC_out <= PC_in;
     end
+
 end
 
 endmodule
@@ -43,6 +44,7 @@ integer k;
 reg [31:0] I_Mem [63:0]; //64 words of 32 bits 
 
 always @(posedge clk or posedge reset) begin
+
     if(reset)begin
         for (k=0;k<64 ;k = k+1 ) begin
             I_Mem[k] = 32'b0;
@@ -50,8 +52,38 @@ always @(posedge clk or posedge reset) begin
     end else begin
         instruction_out <= I_Mem[read_address];
     end
-    
 end
     
 endmodule
 
+// Register File 
+module Reg_File (
+    clk, reset, reg_write, Rs1, Rs2, Rd, write_data, read_data1, read_data2
+);
+
+input          clk, reset, reg_write;
+input   [4:0]  Rs1, Rs2, Rd;
+input   [31:0] write_data;
+output  [31:0] read_data1, read_data2;
+
+integer k;
+
+reg [31:0] Registers [31:0];
+
+always @(posedge clk or posedge reset) begin
+
+    if(reset) begin
+        for (k=0 ;k<32 ;k=k+1 ) begin
+            Registers[k] <= 32'b0;
+        end
+    end    
+
+    else if (reg_write && Rd != 5'b00000) begin //Never write into x0 
+        Registers[Rd] <= write_data;
+    end
+end
+
+assign read_data1 = Registers[Rs1];
+assign read_data2 = Registers[Rs2];
+
+endmodule
