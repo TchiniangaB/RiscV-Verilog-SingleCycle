ea9c95f549aea3daeabe9129f47c1c33718aac22
babaye06 <bastientchinianga@gmail.com>
2025-05-01 03:06:55 +0200

    modif

--- src/riscV_cpu.v
+++ src/riscV_cpu.v
@@ -2,7 +2,17 @@
 module top (
     clk, reset
 );
+
 input clk, reset;
+
+//PC
+program_counter PC(.clk(clk), .reset(), .PC_in(), .PC_out());
+
+//PC Adder
+pc_plus_four PC_Adder(.from_PC(), .next_to_PC());
+
+//Instruction Memory
+instruction_mem Inst_mem(.clk(), .reset(), .read_address(), .instruction_out());
 
 
 endmodule
